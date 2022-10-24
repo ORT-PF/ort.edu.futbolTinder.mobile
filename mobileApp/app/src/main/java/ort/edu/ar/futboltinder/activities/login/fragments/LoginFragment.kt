bb4a2af039b282ee9f54a42122a9108a250df7c8
Franco Redoni <franco.redoni@gmail.com>
2022-10-24 01:16:20 -0300

    Fix

--- mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/login/fragments/LoginFragment.kt
+++ mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/login/fragments/LoginFragment.kt
@@ -9,15 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import okhttp3.internal.wait
 import ort.edu.ar.futboltinder.R
 import ort.edu.ar.futboltinder.activities.home.HomeActivity
 import ort.edu.ar.futboltinder.domain.Login.Forms.UserAuthenticationForm
 import ort.edu.ar.futboltinder.domain.Login.Responses.UserAuthenticationResponse
 import ort.edu.ar.futboltinder.services.APIServices.RetrofitClientBuilder
 import ort.edu.ar.futboltinder.services.APIServices.RetrofitContracts.Authentication.RetrofitAuthenticationService
-import ort.edu.ar.futboltinder.services.AuthenticationService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,7 +24,6 @@ class LoginFragment : Fragment() {
     lateinit var userText : EditText
     lateinit var passwordText : EditText
     lateinit var loginButton : Button
-    private val authenticationService = AuthenticationService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
