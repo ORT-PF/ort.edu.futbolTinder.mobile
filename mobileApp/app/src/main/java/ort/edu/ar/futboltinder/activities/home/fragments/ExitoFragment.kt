497cdc82c06c67f54a8846c6ab43eef61b1a4c0e
Sandra <sandra.albin@hotmail.com>
2022-10-25 00:35:09 -0300

    Flujo de Creación de Partidos

--- mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/home/fragments/ExitoFragment.kt
+++ mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/home/fragments/ExitoFragment.kt
@@ -6,38 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import androidx.navigation.findNavController
 import ort.edu.ar.futboltinder.R
-import ort.edu.ar.futboltinder.activities.login.fragments.RegistrationFragmentDirections
 
 
-class MatchCreatorFragment : Fragment() {
+class ExitoFragment : Fragment() {
 
     lateinit var vista : View
-    lateinit var matchCreateButton : Button
-    lateinit var txtInput: EditText
+    lateinit var backButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        vista = inflater.inflate(R.layout.fragment_match_creator, container, false)
-
-        matchCreateButton = vista.findViewById(R.id.createMatchButton)
+        vista = inflater.inflate(R.layout.fragment_exito, container, false)
+        backButton = vista.findViewById(R.id.backButton)
 
         return vista
     }
 
     override fun onStart(){
         super.onStart()
-        matchCreateButton.setOnClickListener {
-                val action = MatchCreatorFragmentDirections.actionMatchCreatorFragmentToExitoFragment()
-                vista.findNavController().navigate(action)
-            }
+
+        backButton.setOnClickListener(){
+            val action = ExitoFragmentDirections.actionExitoFragmentToMatchBaseFragment()
+            vista.findNavController().navigate(action)
+
         }
 
+    }
 
 
 }
---
+++ mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/home/fragments/MatchBaseFragment.kt
@@ -0,0 +1,20 @@
+package ort.edu.ar.futboltinder.activities.home.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ort.edu.ar.futboltinder.R
+
+class MatchBaseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_match_base, container, false)
+    }
+
+}
