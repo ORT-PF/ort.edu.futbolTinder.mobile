496d7c64498554f1b5a7b7be3f447573f81dca6b
Sandra <sandra.albin@hotmail.com>
2022-11-15 20:54:24 -0300

    implementación hamburger

--- mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/login/LoginActivity.kt
+++ mobileApp/app/src/main/java/ort/edu/ar/futboltinder/activities/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package ort.edu.ar.futboltinder.activities.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ort.edu.ar.futboltinder.R
+import ort.edu.ar.futboltinder.activities.home.HomeActivity
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+
+
+
 }
+
+
